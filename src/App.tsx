89f1410651978a9de564b2df9fb8f7897369526d
Issa <issa.diallo@lemonlearning.fr>
2022-10-11 23:52:48 +0200

    :art: Add bootstrap.min.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import PageNotFound from './pages/PageNotFound'
